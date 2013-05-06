c655c76600979337ce8de21d8eb1ad34328734fb
Raphael Jardim Lopes <raphaeljlps@gmail.com>
2013-05-05 22:37:18 -0300

    Register Client View working

--- src/br/edu/utfpr/students/control/RegisterPanelController.java
+++ src/br/edu/utfpr/students/control/RegisterPanelController.java
@@ -5,7 +5,19 @@ package br.edu.utfpr.students.control;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
+import javax.swing.JOptionPane;
+
+import br.edu.utfpr.students.model.Address;
+import br.edu.utfpr.students.model.Client;
+import br.edu.utfpr.students.model.Contact;
+import br.edu.utfpr.students.persistence.DAOFactory;
+import br.edu.utfpr.students.persistence.interfaces.ClientDAO;
 import br.edu.utfpr.students.view.RegisterPanel;
 
 /**
@@ -34,18 +46,98 @@ public class RegisterPanelController implements ActionListener {
 
 		MainControl.getRcp().getBtnClientCancel().addActionListener(this);
 		MainControl.getRcp().getBtnClientCancel().setActionCommand("cancel");
+		
+		MainControl.getRcp().getBtnOk().addActionListener(this);
+		MainControl.getRcp().getBtnOk().setActionCommand("createClient");
 
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		switch (e.getActionCommand()) {
-		case "cancel":
-			mainControl.getMwpc();
-			break;
+			case "cancel":
+				mainControl.getMwpc();
+				break;
+			case "createClient":
+				boolean result = registerClient();
+				if(result){
+					JOptionPane.showMessageDialog(null, "Client registered");
+					cleanClientView();
+				}else{
+					JOptionPane.showMessageDialog(null, "Client couldn't be registered");
+				}
+				//exibir msg se foi bem sucedida
+				break;
+				default:
+				break;
 		}
 
 	}
+	
+	public void cleanClientView(){
+		MainControl.getRcp().getTextFieldBirthdate().setText("");
+		MainControl.getRcp().getTextFieldCellphone().setText("");
+		MainControl.getRcp().getTextFieldCity().setText("");
+		MainControl.getRcp().getTextFieldContact().setText("");
+		MainControl.getRcp().getTextFieldCountry().setText("");
+		MainControl.getRcp().getTextFieldDistrict().setText("");
+		MainControl.getRcp().getTextFieldEmail().setText("");
+		MainControl.getRcp().getTextFieldName().setText("");
+		MainControl.getRcp().getTextFieldNumber().setText("");
+		MainControl.getRcp().getTextFieldSSN().setText("");
+		MainControl.getRcp().getTextFieldState().setText("");
+		MainControl.getRcp().getTextFieldStreet().setText("");
+		MainControl.getRcp().getTextFieldTelephone().setText("");
+		MainControl.getRcp().getTextFieldZipcode().setText("");
+		
+	}
+	
+	public boolean registerClient(){
+		boolean sucess = true;
+		//ALTERAR PARA QUANDO FOR FILTRAR NO MODEL
+		Client client = new Client();
+		client.setName(MainControl.getRcp().getTextFieldName().getText());
+		client.setSsn(MainControl.getRcp().getTextFieldSSN().getText());
+		
+		Address end = new Address();
+		end.setZipCode(MainControl.getRcp().getTextFieldZipcode().getText());
+		end.setStreet(MainControl.getRcp().getTextFieldStreet().getText());
+		end.setDistrict(MainControl.getRcp().getTextFieldDistrict().getText());
+		end.setCity(MainControl.getRcp().getTextFieldCity().getText());
+		end.setState(MainControl.getRcp().getTextFieldState().getText());
+		end.setCountry(MainControl.getRcp().getTextFieldCountry().getText());
+		end.setNumber(Integer.parseInt(MainControl.getRcp().getTextFieldNumber().getText()));
+		client.setAddr(end);
+		
+		Contact contact = new Contact();
+		contact.setEmail(MainControl.getRcp().getTextFieldEmail().getText());
+		contact.setCellphone(MainControl.getRcp().getTextFieldCellphone().getText());
+		contact.setTelephone(MainControl.getRcp().getTextFieldTelephone().getText());
+		client.setCont(contact);
+		Date date = null;
+		try {
+			date  = new SimpleDateFormat("dd/MM/yyyy").parse(MainControl.getRcp().getTextFieldBirthdate().getText());
+		} catch (ParseException e) {
+			sucess = false;
+			e.printStackTrace();
+		}
+		
+		client.setBirthdate(Calendar.getInstance());
+		client.getBirthdate().setTime(date);
+		
+		DAOFactory post = DAOFactory.getDAOFactory(DAOFactory.POSTGRES);
+		ClientDAO cdao = post.getClientDAO();
+		
+		try {
+			cdao.insertClient(client);
+		} catch (ClassNotFoundException | SQLException e) {
+			sucess = false;
+			e.printStackTrace();
+		}
+		return sucess;
+		
+	}
+	
 
 	public RegisterPanel getView() {
 		return view;
--- src/br/edu/utfpr/students/view/RegisterClientPanel.java
+++ src/br/edu/utfpr/students/view/RegisterClientPanel.java
@@ -22,7 +22,16 @@ public class RegisterClientPanel extends JPanel {
 	private JTextField textFieldCity;
 	private JTextField textFieldNumber;
 	private JTextField textFieldBirthdate;
-	private JTextField textFieldContact;
+	private JTextField textFieldEmail;
+	private JTextField textFieldTelephone;
+	private JTextField textFieldCellphone;
+	private JLabel lblCellphone;
+	private JTextField textFieldZipcode;
+	private JLabel lblZipCode;
+	private JTextField textFieldDistrict;
+	private JLabel lblDistrict;
+	private JLabel lblCountry;
+	private JTextField textFieldCountry;
 
 	/**
 	 * Create the panel.
@@ -42,7 +51,7 @@ public class RegisterClientPanel extends JPanel {
 		add(btnClientCancel);
 
 		JLabel lblName = new JLabel("Name");
-		lblName.setBounds(36, 60, 56, 16);
+		lblName.setBounds(47, 60, 56, 16);
 		add(lblName);
 
 		textFieldName = new JTextField();
@@ -51,20 +60,20 @@ public class RegisterClientPanel extends JPanel {
 		textFieldName.setColumns(10);
 
 		JLabel lblAdress = new JLabel("Adress:");
-		lblAdress.setBounds(36, 293, 56, 16);
+		lblAdress.setBounds(47, 329, 56, 16);
 		add(lblAdress);
 
 		JLabel lblStreet = new JLabel("Street");
-		lblStreet.setBounds(36, 451, 56, 16);
+		lblStreet.setBounds(47, 461, 56, 16);
 		add(lblStreet);
 
 		textFieldStreet = new JTextField();
-		textFieldStreet.setBounds(127, 448, 175, 22);
+		textFieldStreet.setBounds(127, 455, 168, 22);
 		add(textFieldStreet);
 		textFieldStreet.setColumns(10);
 
 		JLabel lblSsn = new JLabel("SSN");
-		lblSsn.setBounds(36, 116, 56, 16);
+		lblSsn.setBounds(47, 116, 56, 16);
 		add(lblSsn);
 
 		textFieldSSN = new JTextField();
@@ -73,34 +82,34 @@ public class RegisterClientPanel extends JPanel {
 		textFieldSSN.setColumns(10);
 
 		JLabel lblState = new JLabel("State");
-		lblState.setBounds(36, 347, 56, 16);
+		lblState.setBounds(352, 415, 56, 16);
 		add(lblState);
 
 		textFieldState = new JTextField();
-		textFieldState.setBounds(127, 344, 175, 22);
+		textFieldState.setBounds(432, 409, 175, 22);
 		add(textFieldState);
 		textFieldState.setColumns(10);
 
 		JLabel lblNewLabel = new JLabel("City");
-		lblNewLabel.setBounds(36, 399, 56, 16);
+		lblNewLabel.setBounds(47, 409, 56, 16);
 		add(lblNewLabel);
 
 		textFieldCity = new JTextField();
-		textFieldCity.setBounds(127, 396, 175, 22);
+		textFieldCity.setBounds(127, 403, 168, 22);
 		add(textFieldCity);
 		textFieldCity.setColumns(10);
 
 		JLabel lblNumber = new JLabel("Number");
-		lblNumber.setBounds(36, 504, 56, 16);
+		lblNumber.setBounds(47, 514, 56, 16);
 		add(lblNumber);
 
 		textFieldNumber = new JTextField();
-		textFieldNumber.setBounds(127, 501, 175, 22);
+		textFieldNumber.setBounds(127, 511, 168, 22);
 		add(textFieldNumber);
 		textFieldNumber.setColumns(10);
 
 		JLabel lblBirthdate = new JLabel("Birthdate");
-		lblBirthdate.setBounds(36, 174, 56, 16);
+		lblBirthdate.setBounds(47, 174, 56, 16);
 		add(lblBirthdate);
 
 		textFieldBirthdate = new JTextField();
@@ -108,14 +117,63 @@ public class RegisterClientPanel extends JPanel {
 		add(textFieldBirthdate);
 		textFieldBirthdate.setColumns(10);
 
-		JLabel lblContact = new JLabel("Contact");
-		lblContact.setBounds(36, 231, 56, 16);
+		JLabel lblContact = new JLabel("Contact:");
+		lblContact.setBounds(47, 202, 56, 16);
 		add(lblContact);
 
-		textFieldContact = new JTextField();
-		textFieldContact.setBounds(127, 228, 175, 22);
-		add(textFieldContact);
-		textFieldContact.setColumns(10);
+		textFieldEmail = new JTextField();
+		textFieldEmail.setBounds(127, 228, 175, 22);
+		add(textFieldEmail);
+		textFieldEmail.setColumns(10);
+
+		JLabel lblEmail = new JLabel("E-mail");
+		lblEmail.setBounds(47, 230, 61, 16);
+		add(lblEmail);
+
+		textFieldTelephone = new JTextField();
+		textFieldTelephone.setBounds(127, 269, 175, 28);
+		add(textFieldTelephone);
+		textFieldTelephone.setColumns(10);
+
+		JLabel lblTelephone = new JLabel("Telephone");
+		lblTelephone.setBounds(47, 281, 66, 16);
+		add(lblTelephone);
+
+		textFieldCellphone = new JTextField();
+		textFieldCellphone.setBounds(432, 269, 175, 28);
+		add(textFieldCellphone);
+		textFieldCellphone.setColumns(10);
+
+		lblCellphone = new JLabel("Cellphone");
+		lblCellphone.setBounds(352, 281, 79, 16);
+		add(lblCellphone);
+
+		textFieldZipcode = new JTextField();
+		textFieldZipcode.setBounds(435, 508, 168, 28);
+		add(textFieldZipcode);
+		textFieldZipcode.setColumns(10);
+
+		lblZipCode = new JLabel("Zip code");
+		lblZipCode.setBounds(350, 514, 61, 16);
+		add(lblZipCode);
+
+		textFieldDistrict = new JTextField();
+		textFieldDistrict.setBounds(435, 452, 168, 28);
+		add(textFieldDistrict);
+		textFieldDistrict.setColumns(10);
+
+		lblDistrict = new JLabel("District");
+		lblDistrict.setBounds(350, 461, 61, 16);
+		add(lblDistrict);
+
+		lblCountry = new JLabel("Country");
+		lblCountry.setBounds(36, 357, 61, 16);
+		add(lblCountry);
+
+		textFieldCountry = new JTextField();
+		textFieldCountry.setBounds(127, 351, 168, 28);
+		add(textFieldCountry);
+		textFieldCountry.setColumns(10);
 
 	}
 
@@ -159,6 +217,48 @@ public class RegisterClientPanel extends JPanel {
 	}
 
 	public JTextField getTextFieldContact() {
-		return textFieldContact;
+		return textFieldEmail;
+	}
+
+	/**
+	 * @return the textFieldEmail
+	 */
+	public JTextField getTextFieldEmail() {
+		return textFieldEmail;
+	}
+
+	/**
+	 * @return the textFieldTelephone
+	 */
+	public JTextField getTextFieldTelephone() {
+		return textFieldTelephone;
+	}
+
+	/**
+	 * @return the textFieldCellphone
+	 */
+	public JTextField getTextFieldCellphone() {
+		return textFieldCellphone;
+	}
+
+	/**
+	 * @return the textFieldDistrict
+	 */
+	public JTextField getTextFieldDistrict() {
+		return textFieldDistrict;
+	}
+
+	/**
+	 * @return the textFieldZipcode
+	 */
+	public JTextField getTextFieldZipcode() {
+		return textFieldZipcode;
+	}
+
+	/**
+	 * @return the textFieldCountry
+	 */
+	public JTextField getTextFieldCountry() {
+		return textFieldCountry;
 	}
 }
